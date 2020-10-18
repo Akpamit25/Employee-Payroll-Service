b9353e99a3c0413b9b3ebac6fef476937c3fbf4e
Amit Kumar <amitnitsri25@gmail.com>
2020-10-18 20:14:54 +0530

    This Is Master Branch

---
+++ src/main/java/com/EmployeePayroll/EmployeePayrollData.java
@@ -0,0 +1,15 @@
+package com.EmployeePayroll;
+
+public class EmployeePayrollData {
+
+	int id;
+	String name;
+	float salary;
+
+	public EmployeePayrollData(int id, String name, float salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+}
---
+++ src/main/java/com/EmployeePayroll/EmployeePayrollMain.java
@@ -0,0 +1,11 @@
+package com.EmployeePayroll;
+
+import java.util.*;
+
+public class EmployeePayrollMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To  My Git Repository");
+
+	}
+}
